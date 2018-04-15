97124e98f4325738ddbf5e20ca3c35318ec6bf30
Christiana Routon <christiana.routon@gmail.com>
2018-04-15 16:00:21 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('server/public'));
+
+let env = require ('dotenv');
+env.config();
+
+let port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log('up and running on port', port);
+});
---
+++ todo.sql
@@ -0,0 +1,5 @@
+CREATE TABLE todo(
+    id serial primary key,
+    task varchar(280),
+	completed boolean
+);
